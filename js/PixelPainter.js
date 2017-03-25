68c671a9d8ab49895131e143d624f21b8cf5df2c
GaganTut <gstut@live.com>
2017-03-24 19:14:39 -1000

    Can add colors

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -5,6 +5,7 @@ window.onload = function() {
 
   let colorContainer = new GridContainer(100, 100);
   let colorPal = new ColorPalette(colorContainer.element);
+  let colorPicker = new ColorPicker();
 
 
   document.querySelector("#pixelPainter").appendChild(grid.container);
@@ -12,9 +13,23 @@ window.onload = function() {
 
   (function giveColors() {
     let colorArray = ["red", "blue", "green", "black", "white", "orange", "yellow", "gray", "purple", "light-blue", "darkgray", "indianred", "cyan", "brown", "Beige", "Gold", "Pink", "Indigo", "Lime", "maroon", "Navy", "Orchid", "Peru", "RoyalBlue", "Silver"];
+
     let colorDivs = document.querySelectorAll(".colorDivs");
+    let gridDivs = document.querySelectorAll(".gridDivs");
+
     for (var i = 0; i < colorDivs.length; i++) {
       colorDivs[i].style["background-color"] = colorArray[i];
+      colorDivs[i].addEventListener("click", function() {
+        colorPicker.color = this.style["background-color"];
+        console.log(colorPicker.color);
+      });
+    }
+    for (var i = 0; i < gridDivs.length; i++) {
+      gridDivs[i].addEventListener("click", function() {
+        this.style["background-color"] = colorPicker.color;
+        console.log(colorPicker.color);
+      });
     }
   })();
+
 };

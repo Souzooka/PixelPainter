98ca6d25ad5cb8a3cbd5735a51a3e8aaa1f870b4
GaganTut <gstut@live.com>
2017-03-24 18:23:51 -1000

    Fixed borders

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
-  let gridContainer = new GridContainer(1000, 1000);
-  let grid = new Grid(100, 100, gridContainer.element);
+  let gridContainer = new GridContainer(500, 500);
+  let grid = new Grid(50, 50, gridContainer.element);
 
 
   document.querySelector("#pixelPainter").appendChild(grid.container);

6455c74655378eec612058ae36473446ed7c3a18
Dakota <bazookaharwell@gmail.com>
2017-03-24 17:04:58 -1000

    I added a box

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,10 @@
 window.onload = function() {
 
   let grid = new Grid();
+  let gridContainer = new GridContainer(800, 800);
+
+  document.querySelector("#pixelPainter").appendChild(gridContainer.element);
+
+
 
 };

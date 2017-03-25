be72ede17f46a6cd7ec0ebb2d2cc23ec8345e890
GaganTut <gstut@live.com>
2017-03-24 19:00:50 -1000

    Created color palette

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -10,4 +10,11 @@ window.onload = function() {
   document.querySelector("#pixelPainter").appendChild(grid.container);
   document.querySelector("#pixelPainter").appendChild(colorPal.container);
 
+  (function giveColors() {
+    let colorArray = ["red", "blue", "green", "black", "white", "orange", "yellow", "gray", "purple", "light-blue", "darkgray", "indianred", "cyan", "brown", "Beige", "Gold", "Pink", "Indigo", "Lime", "maroon", "Navy", "Orchid", "Peru", "RoyalBlue", "Silver"];
+    let colorDivs = document.querySelectorAll(".colorDivs");
+    for (var i = 0; i < colorDivs.length; i++) {
+      colorDivs[i].style["background-color"] = colorArray[i];
+    }
+  })();
 };

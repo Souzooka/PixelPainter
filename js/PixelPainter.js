4f2aa32685deb72da1e39241f5d2a2e44752aa9c
GaganTut <gstut@live.com>
2017-03-24 18:17:01 -1000

    Created dynamic rows

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
-  let gridContainer = new GridContainer(800, 800);
-  let grid = new Grid(10, 10, gridContainer.element);
+  let gridContainer = new GridContainer(1000, 1000);
+  let grid = new Grid(100, 100, gridContainer.element);
 
 
   document.querySelector("#pixelPainter").appendChild(grid.container);

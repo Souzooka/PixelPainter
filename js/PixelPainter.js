92a7df3fc993196e9c95cdf31e6f3ca5e19a7c9f
GaganTut <gstut@live.com>
2017-03-25 10:23:45 -1000

    New div for custome inputs

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -5,19 +5,26 @@
   let gridFuncs = gridFunctions();
 
   let colorContainer = new GridContainer(100, 100);
-  let colorPal = new ColorPalette(colorContainer.element);
+  let colorPal = new ColorPalette(colorContainer.element, "colorDivs");
   let colorPicker = new ColorPicker();
 
   document.querySelector("#pixelPainter").appendChild(grid.container);
   document.querySelector("#pixelPainter").appendChild(colorPal.container);
 
-  paletteStyle();
+  paletteStyle(".colorDivs");
+
   gridFuncs.style();
 
   let clearButton = new ClearButton(document.querySelectorAll(".gridDivs"), "Clear Grid");
   pixelPainter.appendChild(clearButton.element);
 
-  let colorInput = new ColorInput(colorPicker, "Save Color");
+  let colorInput = new ColorInput(colorPicker, "Use Color");
   pixelPainter.appendChild(colorInput.element);
 
   inputColors();
+
+  let customPaletteBox = new GridContainer(100, 100);
+  let customPalette = new ColorPalette(customPaletteBox.element, "customPal");
+  document.querySelector("#pixelPainter").appendChild(customPaletteBox.element);
+  console.log(customPaletteBox.element);
+  paletteStyle(".customPal", ["white"]);

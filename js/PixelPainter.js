cee72da40b7065e8196b52a434a9cec3af6b1108
GaganTut <gstut@live.com>
2017-03-24 18:43:16 -1000

    Created second div grid for color palete

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,9 +3,11 @@ window.onload = function() {
   let gridContainer = new GridContainer(500, 500);
   let grid = new Grid(50, 50, gridContainer.element);
 
+  let colorContainer = new GridContainer(100, 100);
+  let colorPal = new ColorPalette(colorContainer.element);
+
 
   document.querySelector("#pixelPainter").appendChild(grid.container);
-
-
+  document.querySelector("#pixelPainter").appendChild(colorPal.container);
 
 };

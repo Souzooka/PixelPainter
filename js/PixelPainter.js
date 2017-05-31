4598a0b152508f1d004c5410e76b20f2749e47e7
Souzooka <bazookaharwell@gmail.com>
2017-05-30 22:25:37 -1000

    Start refactor, create Pixel class

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,2 +1,6 @@
+/*jshint esversion:6*/
 'use strict';
-let newPixelPainter = new PixelPainter("#pixelPainter", 600, 600, 50, 50);
+
+console.log(new Pixel(240, 30, 200).getColorCode());
+//const pixelPainter = new PixelPainter("#pixelPainter", 600, 600, 50, 50);
+//document.querySelector('#pixel-painter-container').appendChild(pixelPainter.element);

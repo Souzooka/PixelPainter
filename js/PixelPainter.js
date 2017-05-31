7c4fb4c338c4b0ca3e5ed93c5b749bab518449e8
Souzooka <bazookaharwell@gmail.com>
2017-05-30 22:58:05 -1000

    Add initialColor param to Image

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,6 +2,6 @@
 'use strict';
 
 console.log(new Pixel(240, 30, 200).getColorCode());
-console.log(new Image(5, 20));
+console.log(new Image(5, 20, {r: 240, g: 30, b: 200}));
 //const pixelPainter = new PixelPainter("#pixelPainter", 600, 600, 50, 50);
 //document.querySelector('#pixel-painter-container').appendChild(pixelPainter.element);

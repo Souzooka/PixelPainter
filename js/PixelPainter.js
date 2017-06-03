960096299b4b4af7a4944827359a09a3dce41224
Dakota <bazookaharwell@gmail.com>
2017-06-02 17:11:54 -1000

    Implement grid class to hold DOM elements

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,5 +3,5 @@
 
 console.log(new Pixel(240, 30, 200).getColorCode());
 console.log(new Image(5, 20, {r: 240, g: 30, b: 200}));
-//const pixelPainter = new PixelPainter("#pixelPainter", 600, 600, 50, 50);
-//document.querySelector('#pixel-painter-container').appendChild(pixelPainter.element);
+const grid = new Grid(10, 10, 600, 600);
+document.querySelector('#pixel-painter-container').appendChild(grid.containerElement);
